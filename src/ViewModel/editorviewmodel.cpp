17bada339f556d1a208bf733b20ade8009738dfb
Antony C <antony.coco@live.fr>
2023-09-05 12:47:11 +0200

    Mise à jour des fichiers

---
+++ src/ViewModel/editorviewmodel.cpp
@@ -0,0 +1,6 @@
+#include "editorviewmodel.h"
+
+EditorViewModel::EditorViewModel()
+{
+
+}
---
+++ src/ViewModel/editorviewmodel.h
@@ -0,0 +1,11 @@
+#ifndef EDITORVIEWMODEL_H
+#define EDITORVIEWMODEL_H
+
+
+class EditorViewModel
+{
+public:
+    EditorViewModel();
+};
+
+#endif // EDITORVIEWMODEL_H
